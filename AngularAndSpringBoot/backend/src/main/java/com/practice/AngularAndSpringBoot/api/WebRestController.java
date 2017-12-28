a361349139ef4a806f8a0c15c483571d2f2ebf29
HENRY-HSU\user <henry.hsu@hy-tech.com.tw>
2017-12-28 10:26:05 +0800

    [DEV][分類 : forntend + backend] [開發名稱 : 前端可以8080的api] 設定proxy 檔案使得 前端可從4200打到8080的api

---
+++ AngularAndSpringBoot/backend/src/main/java/com/practice/AngularAndSpringBoot/api/WebRestController.java
@@ -0,0 +1,13 @@
+package com.practice.AngularAndSpringBoot.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+	
+	@RequestMapping("/api/hi")
+	public String hi(){
+		return "Hello World From Backend!";
+	}
+}
