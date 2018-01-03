31138ec5b01ceae86f36866ebc00cbd32cd9f3bd
HENRY-HSU\user <henry.hsu@hy-tech.com.tw>
2018-01-03 18:29:30 +0800

    [DEV][分類 : CRM] [開發名稱 : CRM] 刻版

--- AngularAndSpringBoot/frontend/src/app/app-routing.module.ts
+++ AngularAndSpringBoot/frontend/src/app/app-routing.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HerosComponent } from './heros/heros.component';
-import { DashboardComponent } from './dashboard/dashboard.component'
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'
-import { TutorialLinkComponent } from './tutorial-link/tutorial-link.component'
+import { HerosComponent } from './tour-of-heroes/heros/heros.component';
+import { DashboardComponent } from './tour-of-heroes/dashboard/dashboard.component'
+import { HeroDetailComponent } from './tour-of-heroes/hero-detail/hero-detail.component';
+import { TutorialLinkComponent } from './tour-of-heroes/tutorial-link/tutorial-link.component';
+import { TourOfHeroesComponent } from './tour-of-heroes/tour-of-heroes.component';
+import { CrmComponent } from './crm/crm.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HerosComponent },
-  { path: 'turtorial', component: TutorialLinkComponent }
+  { path: 'CRM', component: CrmComponent },
+  {
+    path: 'tourOfHero',
+    component: TourOfHeroesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'detail/:id', component: HeroDetailComponent },
+      { path: 'heroes', component: HerosComponent },
+      { path: 'turtorial', component: TutorialLinkComponent },
+    ]
+  },
+  { path: '**', redirectTo: '/tourOfHero', pathMatch: 'full' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularAndSpringBoot/frontend/src/app/app.module.ts
+++ AngularAndSpringBoot/frontend/src/app/app.module.ts
@@ -4,19 +4,23 @@ import { FormsModule } from '@angular/forms'; //<-- ngModel lives here
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppRoutingModule } from './/app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { HerosComponent } from './heros/heros.component';
-import { MessagesComponent } from './messages/messages.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TutorialLinkComponent } from './tutorial-link/tutorial-link.component';
-
-import { HeroService } from './heros/service/hero.service';
-import { MessageService } from './messages/service/message.service';
-import { InMemoryDataService } from './in-memory-data.service';
-import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HerosComponent } from './tour-of-heroes/heros/heros.component';
+import { MessagesComponent } from './tour-of-heroes/messages/messages.component';
+import { HeroDetailComponent } from './tour-of-heroes/hero-detail/hero-detail.component';
+import { DashboardComponent } from './tour-of-heroes/dashboard/dashboard.component';
+import { TutorialLinkComponent } from './tour-of-heroes/tutorial-link/tutorial-link.component';
+import { HeroSearchComponent } from './tour-of-heroes/hero-search/hero-search.component';
 import { MemberComponent } from './member/member.component';
+import { TourOfHeroesComponent } from './tour-of-heroes/tour-of-heroes.component';
+import { CrmComponent } from './crm/crm.component';
+
+import { HeroService } from './tour-of-heroes/heros/service/hero.service';
+import { MessageService } from './tour-of-heroes/messages/service/message.service';
+import { InMemoryDataService } from './in-memory-data.service';
+
 
 
 @NgModule({
@@ -28,14 +32,17 @@ import { MemberComponent } from './member/member.component';
     DashboardComponent,
     HeroSearchComponent,
     TutorialLinkComponent,
-    MemberComponent
+    MemberComponent,
+    TourOfHeroesComponent,
+    CrmComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    NgbModule.forRoot(),
     HttpClientModule,
-    
+
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
